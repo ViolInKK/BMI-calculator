960c88f5e2756299c087b3776be56ef3e0449457
violi <violinkk29@gmail.com>
2023-10-29 10:36:04 +0300

    numPudButton styles

--- src/components/NumPudButton.tsx
+++ src/components/NumPudButton.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
 
 const NumPudButton = () => {
   return (
-    <div>NumPudButton</div>
+    <div className="flex justify-center items-center bg-inputButtonBg_1 text-textPrimary_1 rounded border-inputButtonShadow_1 border-b-4 w-[100px] h-[55px]">
+        1
+    </div>
   )
 }
 

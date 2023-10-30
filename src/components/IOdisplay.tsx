55246eef4b47df0e7c0d091e65ca0cb1c78aee22
violi <violinkk29@gmail.com>
2023-10-30 10:19:08 +0300

    IOdisplay styles

--- src/components/IOdisplay.tsx
+++ src/components/IOdisplay.tsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const IOdisplay = () => {
   return (
-    <div>IOdisplay</div>
+    <div className='max-w-[536px] min-h-[120px] flex flex-row-reverse bg-IOdisplayBg_1 rounded text-3xl px-3 py-3 overflow-hidden'>
+      399,198
+    </div>
   )
 }
 

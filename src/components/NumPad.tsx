bfcb09594c441e794fd3d9803652ba59495ef17b
violi <violinkk29@gmail.com>
2023-10-30 10:01:33 +0300

    numpad styles

--- src/components/NumPad.tsx
+++ src/components/NumPad.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import { NumPadButton } from '.'
 
 const NumPad = () => {
   return (
-    <div>NumPad</div>
+    <div className='min-w-[536px] min-h-[435px] bg-buttonsContainerBg_1 grid grid-rows-5 grid-cols-4 grid-flow-col gap-3 p-4 rounded'>
+    </div>
   )
 }
 

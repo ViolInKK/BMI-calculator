8182b533c8b92b93ed6307852aaaf28e54d72829
violi <violinkk29@gmail.com>
2023-10-31 11:34:37 +0300

    Formatted with prettier

--- src/components/NumPadButton.tsx
+++ src/components/NumPadButton.tsx
@@ -1,23 +1,19 @@
+import { typeStyles } from "../constants";
 
-
-import { typeStyles } from "../constants"
-
-
-interface NumPadButtonProps{
-  name: string 
-  type: string
-  isWide: boolean
+interface NumPadButtonProps {
+  name: string;
+  type: string;
+  isWide: boolean;
 }
 
-const NumPadButton = ({name, type, isWide}:NumPadButtonProps) => {
-  
-  const themeNumber = 1
+const NumPadButton = ({ name, type, isWide }: NumPadButtonProps) => {
+  const themeNumber = 1;
 
   return (
-    <div className={`flex justify-center items-center ${typeStyles[themeNumber][type]} rounded border-b-4 ${isWide ? 'col-span-2' : null}`}>
-        {name}
+    <div className={`flex justify-center items-center ${typeStyles[themeNumber][type]} rounded border-b-4 ${isWide ? "col-span-2" : null}`}>
+      {name}
     </div>
-  )
-}
+  );
+};
 
-export default NumPadButton
+export default NumPadButton;
